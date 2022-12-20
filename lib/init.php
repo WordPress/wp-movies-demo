240f1b04b073208545096d5f508b5f47f768e366
Carlos Bravo <carlos.bravo@automattic.com>
2022-12-20 16:58:55 +0100

    Format php according standards

--- lib/init.php
+++ lib/init.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 /**
  * Register the scripts - Temporary until we have the bundling of the Interactivity API ready as a package.
@@ -13,21 +13,22 @@ add_action( 'plugins_loaded', 'wp_directives_loader' );
 /**
  * Add default settings upon activation.
  */
-function wp_directives_activate()
-{
-	add_option('wp_directives_plugin_settings', [
-		'client_side_transitions' => false,
-	]);
+function wp_directives_activate() {
+	add_option(
+		'wp_directives_plugin_settings',
+		array(
+			'client_side_transitions' => false,
+		)
+	);
 }
-register_activation_hook(__FILE__, 'wp_directives_activate');
+register_activation_hook( __FILE__, 'wp_directives_activate' );
 
 /**
  * Delete settings on uninstall.
  */
-function wp_directives_uninstall()
-{
-	delete_option('wp_directives_plugin_settings');
+function wp_directives_uninstall() {
+	delete_option( 'wp_directives_plugin_settings' );
 }
-register_uninstall_hook(__FILE__, 'wp_directives_uninstall');
+register_uninstall_hook( __FILE__, 'wp_directives_uninstall' );
 
 require_once __DIR__ . '/client-side-transitions.php';

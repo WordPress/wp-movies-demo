9ce1240d876073a452dd432555e73610d6ea9194
Michal Czaplinski <mmczaplinski@gmail.com>
2023-01-27 12:02:00 -0500

    Enqueue editor styles

--- theme/functions.php
+++ theme/functions.php
@@ -3,5 +3,9 @@
 function setup_theme() {
 	wp_enqueue_style( 'style', get_stylesheet_uri() );
 }
-
 add_action( 'wp_enqueue_scripts', 'setup_theme' );
+
+function enqueue_editor_styles() {
+	wp_enqueue_style( 'editor-style', get_theme_file_uri( '/editor-style.css' ) );
+}
+add_action( 'enqueue_block_editor_assets', 'enqueue_editor_styles' );

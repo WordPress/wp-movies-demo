35fbb893636d9991d101a56505ded99d39173d8d
Mario Santos <santosguillamot@gmail.com>
2023-02-15 20:52:58 +0100

    Add movie runtime

--- lib/db-update/index.php
+++ lib/db-update/index.php
@@ -111,6 +111,7 @@ function wpmovies_add_movies() {
 			$movie_vote_average = $movie_data->getVoteAverage(); // In scale x/10
 			$movie_vote_count   = $movie_data->getVoteCount();
 			$movie_status       = $movie_data->getStatus();
+			$movie_runtime       = $movie_data->getRuntime();
 			$movie_homepage     = $movie_data->getHomepage();
 			if ( $movie_data->getReleaseDate() ) {
 				$movie_release_date = $movie_data->getReleaseDate()->format( 'Y-m-d' );
@@ -162,6 +163,7 @@ function wpmovies_add_movies() {
 					'_wpmovies_release_date' => $movie_release_date,
 					'_wpmovies_revenue'      => $movie_revenue,
 					'_wpmovies_budget'       => $movie_budget,
+					'_wpmovies_runtime'      => $movie_runtime,
 				),
 			);
 

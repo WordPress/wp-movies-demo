9d38f74c2682cc7aacbab382b2f5087d69babb4f
Mario Santos <santosguillamot@gmail.com>
2023-02-15 20:16:37 +0100

    Add percentage score circle to the edit

--- src/blocks/movie-score/edit.js
+++ src/blocks/movie-score/edit.js
@@ -3,7 +3,42 @@ import { useBlockProps } from '@wordpress/block-editor';
 import './style.css';
 
 const Edit = () => {
-	return <div {...useBlockProps()}>Movie Score</div>;
+	return (
+		<div
+			{...useBlockProps()}
+			class="wpmovies-score-wrap"
+			style={{
+				backgroundColor: '#21d07a55',
+			}}
+		>
+			<div class="wpmovies-score-circle">
+				<div
+					class="wpmovies-score-mask wpmovies-score-full"
+					style={{ transform: 'rotate(135deg)' }}
+				>
+					<div
+						class="wpmovies-score-fill"
+						style={{
+							backgroundColor: '#21d07a',
+							transform: 'rotate(135deg)',
+						}}
+					></div>
+				</div>
+				<div class="wpmovies-score-mask wpmovies-score-half">
+					<div
+						class="wpmovies-score-fill"
+						style={{
+							backgroundColor: '#21d07a',
+							transform: 'rotate(135deg)',
+						}}
+					></div>
+				</div>
+				<div class="wpmovies-score-inside-circle">
+					<p>75%</p>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Edit;

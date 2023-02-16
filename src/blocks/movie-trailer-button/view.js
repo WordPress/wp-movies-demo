b23eaba5e2d9e9e79043b7fa6ef54c75be1db016
Mario Santos <santosguillamot@gmail.com>
2023-02-16 17:59:21 +0100

    Use `dataset` instead of `getAttribute`

--- src/blocks/movie-trailer-button/view.js
+++ src/blocks/movie-trailer-button/view.js
@@ -7,7 +7,7 @@ wpx({
 				state.wpmovies.isPlaying = true;
 				state.wpmovies.currentVideo =
 					'https://www.youtube.com/embed/' +
-					event.target.getAttribute('data-wpmovies-video-id') +
+					event.target.dataset.wpmoviesVideoId +
 					'?autoplay=1';
 			},
 		},

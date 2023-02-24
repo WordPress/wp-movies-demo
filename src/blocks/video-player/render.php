8e76595870d33797378b85553290b97fe1a3f9c6
Michal Czaplinski <mmczaplinski@gmail.com>
2023-02-23 19:08:56 -0500

    Fix video player styling a bit

--- src/blocks/video-player/render.php
+++ src/blocks/video-player/render.php
@@ -1,15 +1,17 @@
 <?php
 $wrapper_attributes = get_block_wrapper_attributes(
-   [
-      'class' => 'wpmovies-video-player'
-   ]
+	array(
+		'class' => 'wpmovies-video-player',
+	)
 );
 ?>
 
 <wp-show when="selectors.wpmovies.isPlaying" <?php echo $wrapper_attributes; ?>>
    <div class="wpmovies-video-wrapper">
-      <span wp-on:click="actions.wpmovies.closeVideo">X</span>
-      <iframe width="420" height="315" allow="autoplay" allowfullscreen wp-bind:src="state.wpmovies.currentVideo">
-      </iframe>
+	  <div class="wpmovies-video-close" >
+		 <button class="close-button" wp-on:click="actions.wpmovies.closeVideo"> Close </button>
+	  </div>
+	  <iframe width="420" height="315" allow="autoplay" allowfullscreen wp-bind:src="state.wpmovies.currentVideo">
+	  </iframe>
    </div>
 </wp-show>

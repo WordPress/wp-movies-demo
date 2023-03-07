c46ffec63b498a79b94384064b42ab78d8fec67b
Mario Santos <santosguillamot@gmail.com>
2023-03-07 19:51:39 +0100

    Fix post-favorite block

--- src/blocks/post-favorite/view.js
+++ src/blocks/post-favorite/view.js
@@ -1,27 +1,26 @@
-import { store } from '../../../../block-hydration-experiments/src/runtime/store.js';
+// import { store } from '../../../../block-hydration-experiments/src/runtime/store.js';
+const { store } = window.wp.interactivity;
 
 store({
-	state: {
-		favorites: {
-			posts: [],
-		},
-	},
 	selectors: {
-		favorites: {
-			isPostIncluded: ({ state, context: { post } }) =>
+		wpmovies: {
+			isMovieIncluded: ({ state, context: { post } }) =>
 				`https://s.w.org/images/core/emoji/14.0.0/svg/${
-					state.favorites.posts.includes(post.id) ? '2764' : '1f90d'
+					state.wpmovies.favoriteMovies.includes(post.id)
+						? '2764'
+						: '1f90d'
 				}.svg`,
 		},
 	},
 	actions: {
-		favorites: {
-			togglePost: ({ state, context }) => {
-				const index = state.favorites.posts.findIndex(
+		wpmovies: {
+			toggleMovie: ({ state, context }) => {
+				const index = state.wpmovies.favoriteMovies.findIndex(
 					(post) => post === context.post.id
 				);
-				if (index === -1) state.favorites.posts.push(context.post.id);
-				else state.favorites.posts.splice(index, 1);
+				if (index === -1)
+					state.wpmovies.favoriteMovies.push(context.post.id);
+				else state.wpmovies.favoriteMovies.splice(index, 1);
 			},
 		},
 	},

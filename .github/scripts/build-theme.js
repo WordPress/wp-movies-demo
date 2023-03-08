c5da507013b18f17c488212194211c1c5a989fc8
Mario Santos <santosguillamot@gmail.com>
2023-03-08 18:37:30 +0100

    Update theme file name

--- .github/scripts/build-theme.js
+++ .github/scripts/build-theme.js
@@ -2,11 +2,11 @@ const fs = require('fs');
 const archiver = require('archiver');
 
 // Create a new zip file
-const output = fs.createWriteStream('wpmovies-theme.zip');
+const output = fs.createWriteStream('wp-movies-theme.zip');
 const archive = archiver('zip', { zlib: { level: 9 } });
 
 // Add files and directories to the zip file
-archive.directory('wp-movies-theme/', 'wpmovies-theme');
+archive.directory('wp-movies-theme/', 'wp-movies-theme');
 
 // Finalize the zip file
 archive.pipe(output);

fd690c9c325ba012a6e46fdd11b2697bdceb1694
Mario Santos <santosguillamot@gmail.com>
2023-03-15 10:47:06 +0100

    Add back the state to post-favorite block

--- src/blocks/post-favorite/render.php
+++ src/blocks/post-favorite/render.php
@@ -4,6 +4,11 @@ $wrapper_attributes = get_block_wrapper_attributes();
 
 store(
 	array(
+		'state'     => array(
+			'wpmovies' => array(
+				'favoriteMovies' => array(),
+			),
+		),
 		'selectors' => array(
 			'wpmovies' => array(
 				'isMovieIncluded' => 'https://s.w.org/images/core/emoji/14.0.0/svg/1f90d.svg',

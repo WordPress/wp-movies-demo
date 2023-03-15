b168df1a4fd737a1360d77129172d4a03691fc21
Mario Santos <santosguillamot@gmail.com>
2023-03-15 16:02:10 +0100

    Change page background styling

--- src/blocks/page-background/render.php
+++ src/blocks/page-background/render.php
@@ -9,16 +9,21 @@ $wrapper_attributes = get_block_wrapper_attributes(
 
 <div <?php echo $wrapper_attributes; ?>>
 	<?php
-	if ( get_post_type() === 'movies' ) {
-		$background_image_id  = get_post_meta( $post->ID, '_wpmovies_backdrop_img_id', true );
-		$background_image_url = wp_get_attachment_image_url( $background_image_id, '' );
-		?>
+	if (get_post_type() === 'movies') {
+		$background_image_id  = get_post_meta($post->ID, '_wpmovies_backdrop_img_id', true);
+		$background_image_url = wp_get_attachment_image_url($background_image_id, '');
+	?>
 		<div class="wpmovies-movie-background">
 			<img src=<?php echo $background_image_url; ?> />
 			<div class="wpmovies-movie-background-shadow"></div>
 		</div>
 
-	<?php } elseif ( get_post_type() === 'actors' ) { ?>
-		<div class="wpmovies-actor-background-shadow"></div>
+	<?php } elseif (get_post_type() === 'actors') {
+		$background_image_url = get_the_post_thumbnail_url();
+	?>
+		<div class="wpmovies-actor-background">
+			<img src=<?php echo $background_image_url; ?> />
+			<div class="wpmovies-actor-background-shadow"></div>
+		</div>
 	<?php } ?>
 </div>

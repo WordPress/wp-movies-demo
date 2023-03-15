4f9bab48c3b0226305a8e70bd8056df563fa6089
Mario Santos <santosguillamot@gmail.com>
2023-03-15 16:32:18 +0100

    Format PHP files

--- src/blocks/actor-birth-place/render.php
+++ src/blocks/actor-birth-place/render.php
@@ -1,7 +1,7 @@
 <?php
 $post               = get_post();
 $wrapper_attributes = get_block_wrapper_attributes();
-$birth_place       = get_post_meta($post->ID, '_wpmovies_actors_place_of_birth', true);
+$birth_place        = get_post_meta( $post->ID, '_wpmovies_actors_place_of_birth', true );
 ?>
 
 <span <?php echo $wrapper_attributes; ?>>

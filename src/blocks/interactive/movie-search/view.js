88499a8de8c7f3658c681cb0a20125c68d8658c0
Mario Santos <santosguillamot@gmail.com>
2024-03-06 15:28:31 +0100

    Update movie search block

--- src/blocks/interactive/movie-search/view.js
+++ src/blocks/interactive/movie-search/view.js
@@ -1,18 +1,20 @@
 import { getElement, store } from '@wordpress/interactivity';
-import { actions as routerActions } from '@wordpress/interactivity-router';
 
-const updateURL = async (value) => {
+const updateURL = async (event, value) => {
 	const url = new URL(window.location);
 	url.searchParams.set('post_type', 'movies');
 	url.searchParams.set('orderby', 'name');
 	url.searchParams.set('order', 'asc');
 	url.searchParams.set('s', value);
-	await routerActions.navigate(`/${url.search}${url.hash}`);
+	await store('core/router').actions.navigate(
+		event,
+		`/${url.search}${url.hash}`
+	);
 };
 
 const { state } = store('wpmovies', {
 	actions: {
-		*updateSearch() {
+		*updateSearch(event) {
 			const { ref } = getElement();
 			const { value } = ref;
 			// Don't navigate if the search didn't really change.
@@ -22,10 +24,10 @@ const { state } = store('wpmovies', {
 
 			if (value === '') {
 				// If the search is empty, navigate to the home page.
-				yield routerActions.navigate('/');
+				yield store('core/router').actions.navigate(event, '/');
 			} else {
 				// If not, navigate to the new URL.
-				yield updateURL(value);
+				yield updateURL(event, value);
 			}
 		},
 	},

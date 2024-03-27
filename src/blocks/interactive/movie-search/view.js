3abd740fc1a60bb9ee97ac7879ab3ef07c2590b4
Mario Santos <santosguillamot@gmail.com>
2024-03-27 11:32:49 +0100

    Update search block to match latest changes in Gutenberg

--- src/blocks/interactive/movie-search/view.js
+++ src/blocks/interactive/movie-search/view.js
@@ -3,21 +3,18 @@
  */
 import { getElement, store } from '@wordpress/interactivity';
 
-const updateURL = async (event, value) => {
+const updateURL = async (value) => {
 	const url = new URL(window.location);
 	url.searchParams.set('post_type', 'movies');
 	url.searchParams.set('orderby', 'name');
 	url.searchParams.set('order', 'asc');
 	url.searchParams.set('s', value);
-	await store('core/router').actions.navigate(
-		event,
-		`/${url.search}${url.hash}`
-	);
+	await store('core/router').actions.navigate(`/${url.search}${url.hash}`);
 };
 
 const { state } = store('wpmovies', {
 	actions: {
-		*updateSearch(event) {
+		*updateSearch() {
 			const { ref } = getElement();
 			const { value } = ref;
 			// Don't navigate if the search didn't really change.
@@ -27,10 +24,10 @@ const { state } = store('wpmovies', {
 
 			if (value === '') {
 				// If the search is empty, navigate to the home page.
-				yield store('core/router').actions.navigate(event, '/');
+				yield store('core/router').actions.navigate('/');
 			} else {
 				// If not, navigate to the new URL.
-				yield updateURL(event, value);
+				yield updateURL(value);
 			}
 		},
 	},

9185fce51f98bbb8f121967690199be24453ae7f
Luis Herranz <luisherranz@gmail.com>
2024-06-04 17:24:50 +0200

    Remove old `data-wp-link` leftover

--- src/blocks/interactive/movie-genres/render.php
+++ src/blocks/interactive/movie-genres/render.php
@@ -14,10 +14,7 @@ $wrapper_attributes = get_block_wrapper_attributes();
 	if ( count( wp_get_post_categories( $post->ID ) ) !== 0 ) {
 		foreach ( wp_get_post_categories( $post->ID ) as $category_id ) {
 			?>
-			<a
-				data-wp-link="{ \'prefetch\': true }"
-				href="<?php echo get_category_link( $category_id ); ?>"
-			>
+			<a href="<?php echo get_category_link( $category_id ); ?>">
 				<?php echo get_cat_name( $category_id ); ?>
 			</a>
 			<?php

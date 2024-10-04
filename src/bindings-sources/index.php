06c1546fb0bd64acc0a1beb6766ad8b489e037f7
Carlos Bravo <37012961+cbravobernal@users.noreply.github.com>
2024-10-04 17:54:27 +0200

    Add movie runtime

--- src/bindings-sources/index.php
+++ src/bindings-sources/index.php
@@ -2,3 +2,4 @@
 require_once __DIR__ . '/actor-birthday.php';
 require_once __DIR__ . '/actor-birth-place.php';
 require_once __DIR__ . '/movie-release-date.php';
+require_once __DIR__ . '/movie-runtime.php';
---
+++ src/bindings-sources/movie-runtime.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Register the actor birthday source.
+ */
+function wp_movies_register_block_bindings_movie_runtime() {
+	register_block_bindings_source(
+		'wpmovies/movie-runtime',
+		array(
+			'label'              => __( 'Movie Runtime', 'wp-movies' ),
+			'uses_context'       => array( 'postId' ),
+			'get_value_callback' => function( $source_args, $block_instance ) {
+				$runtime_minutes    = get_post_meta( $block_instance->context['postId'], '_wpmovies_runtime', true );
+				return intdiv( $runtime_minutes, 60 ) . 'h ' . ( $runtime_minutes % 60 ) . 'm';
+			},
+		)
+	);
+}
+
+add_action( 'init', 'wp_movies_register_block_bindings_movie_runtime' );

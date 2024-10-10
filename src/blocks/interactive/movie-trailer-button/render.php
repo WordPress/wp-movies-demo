76f8a8c2a19e11e37adfe60bcb697ce86c2b069b
Carlos Bravo <37012961+cbravobernal@users.noreply.github.com>
2024-10-10 12:58:45 +0200

    Prevent fatals if post_meta fails

--- src/blocks/interactive/movie-trailer-button/render.php
+++ src/blocks/interactive/movie-trailer-button/render.php
@@ -12,17 +12,21 @@ $wrapper_attributes = get_block_wrapper_attributes(
 $post      = get_post();
 $play_icon = file_get_contents( get_template_directory() . '/assets/play.svg' );
 $videos    = get_post_meta( $post->ID, 'wpmovies_videos', true );
-$trailers  = array_filter(
-	json_decode( $videos, true ),
-	function( $video ) {
-		return 'Trailer' === $video['type'];
-	}
-);
+$trailers  = array();
 
-if ( count( $trailers ) !== 0 ) {
-	$trailer_url = reset( $trailers )['url'];
-	$trailer_id  = substr( $trailer_url, strpos( $trailer_url, '?v=' ) + 3 );
-	$context     = array( 'videoId' => $trailer_id );
+if ( $videos ) {
+	$trailers = array_filter(
+		json_decode( $videos, true ) ?: array(),
+		function( $video ) {
+			return isset( $video['type'] ) && 'Trailer' === $video['type'];
+		}
+	);
+}
+
+if ( $trailers && count( $trailers ) !== 0 ) {
+	 $trailer_url = reset( $trailers )['url'];
+	 $trailer_id  = substr( $trailer_url, strpos( $trailer_url, '?v=' ) + 3 );
+	 $context     = array( 'videoId' => $trailer_id );
 	?>
 
 	<div
@@ -37,7 +41,7 @@ if ( count( $trailers ) !== 0 ) {
 		>
 			<div class="wpmovies-page-button-child">
 				<?php echo $play_icon; ?>
-				<span>Play trailer</span>
+				<span><?php _e( 'Play trailer', 'wpmovies' ); ?></span>
 			</div>
 		</div>
 	</div>

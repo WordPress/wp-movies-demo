5e130bfb352e22c90eac9f8ae8142573cdbed439
Carlos Bravo <37012961+cbravobernal@users.noreply.github.com>
2024-10-11 11:31:39 +0200

    Use early return instead

--- src/blocks/interactive/movie-trailer-button/render.php
+++ src/blocks/interactive/movie-trailer-button/render.php
@@ -14,36 +14,38 @@ $play_icon = file_get_contents( get_template_directory() . '/assets/play.svg' );
 $videos    = get_post_meta( $post->ID, 'wpmovies_videos', true );
 $trailers  = array();
 
-if ( $videos ) {
-	$trailers = array_filter(
-		json_decode( $videos, true ) ?: array(),
-		function( $video ) {
-			return isset( $video['type'] ) && 'Trailer' === $video['type'];
-		}
-	);
+if ( ! $videos ) {
+	return;
 }
 
+$trailers = array_filter(
+	json_decode( $videos, true ) ?: array(),
+	function( $video ) {
+		return isset( $video['type'] ) && 'Trailer' === $video['type'];
+	}
+);
+
 if ( $trailers && count( $trailers ) !== 0 ) {
-	 $trailer_url = reset( $trailers )['url'];
-	 $trailer_id  = substr( $trailer_url, strpos( $trailer_url, '?v=' ) + 3 );
-	 $context     = array( 'videoId' => $trailer_id );
+		$trailer_url = reset( $trailers )['url'];
+		$trailer_id  = substr( $trailer_url, strpos( $trailer_url, '?v=' ) + 3 );
+		$context     = array( 'videoId' => $trailer_id );
 	?>
 
+<div
+	data-wp-interactive="wpmovies"
+	<?php echo $wrapper_attributes; ?>
+	<?php echo wp_interactivity_data_wp_context( $context ); ?>
+>
 	<div
-		data-wp-interactive="wpmovies"
-		<?php echo $wrapper_attributes; ?>
-		<?php echo wp_interactivity_data_wp_context( $context ); ?>
+		class="wpmovies-page-button-parent"
+		data-wp-on--click="actions.setVideo"
+		aria-controls="wp-movies-video-player"
 	>
-		<div
-			class="wpmovies-page-button-parent"
-			data-wp-on--click="actions.setVideo"
-			aria-controls="wp-movies-video-player"
-		>
-			<div class="wpmovies-page-button-child">
-				<?php echo $play_icon; ?>
-				<span><?php _e( 'Play trailer', 'wpmovies' ); ?></span>
-			</div>
+		<div class="wpmovies-page-button-child">
+			<?php echo $play_icon; ?>
+			<span><?php _e( 'Play trailer', 'wpmovies' ); ?></span>
 		</div>
 	</div>
+</div>
 
 <?php } ?>
